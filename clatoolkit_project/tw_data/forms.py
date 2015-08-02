24b6e5ab07ccce2d500856aee8c02176501c9ad1
Aneesha Bakharia <aneesha@Aneeshas-MacBook-Air.local>
2015-08-03 09:46:58 +1000

    Initial code restructure

--- clatoolkit_project/tw_data/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class TwitterGatherForm(forms.Form):
-    account = forms.CharField(label='Account', max_length=50)
-    hashtag = forms.CharField(label='Hashtag', max_length=100)
