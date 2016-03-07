d1355886ec2f6cc7b87a487f6c435a2c02bc9128
Aneesha Bakharia <aneesha@Aneeshas-MacBook-Air.local>
2016-03-07 14:41:51 +1000

    Refactored views.py and urls.py files for dataintegration app

--- clatoolkit_project/dataintegration/urls.py
+++ clatoolkit_project/dataintegration/urls.py
@@ -5,14 +5,12 @@ from dataintegration import views
 
 urlpatterns = patterns(
     url(r'^home/$', views.home, name='home'),
-    url(r'^login/(?P<group_id>\d+)$', views.login, name='login'),
+    #url(r'^login/(?P<group_id>\d+)$', views.login, name='login'),
     url(r'^get_social/$', views.get_social_media_id, name='get_social'),
     url(r'^refreshtwitter/$', views.refreshtwitter, name='refreshtwitter'),
     url(r'^refreshdiigo/$', views.refreshdiigo, name='refreshdiigo'),
     url(r'^refreshforum/$', views.refreshforum, name='refreshforum'),
     url(r'^sendtolrs/$', views.sendtolrs, name='sendtolrs'),
-    url(r'^updatelearningrecords/$', views.updatelearningrecords, name='updatelearningrecords'),
-    url(r'^insertsocialrelationships/$', views.insertsocialrelationships, name='insertsocialrelationships'),
     #url(r'^refreshyoutube/$', views.refreshyoutube, name='refreshyoutube'),
     url(r'^refreshgoogleauthflow/$', views.refreshgoogleauthflow, name='refreshgoogleauthflow'),
     url(r'^ytAuthCallback/$', views.ytAuthCallback, name='ytAuthCallback'),
